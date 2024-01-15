ae872e69430f042eedd96b725986e16b3e984fae
jihyeongeun <hyeongeun0924@gmail.com>
2024-01-15 17:49:00 +0900

    baekjoon_prac_24.01.15

---
+++ Baekjoon_prac/src/main/java/com/ohgiraffers/baekjoon/input_output_1008.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.baekjoon;
+
+import java.util.Scanner;
+
+public class input_output_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        System.out.println(A / B);
+    }
+}
---
+++ Baekjoon_prac/src/main/java/com/ohgiraffers/baekjoon/input_output_2557.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.baekjoon;
+
+public class input_output_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
